eca3c221d75ee07ddb7b00e27ad08eb9f522867b
AkhmadFarizi <farizi2710@gmail.com>
2025-02-01 14:48:32 +0700

    first commit

--- assets/js/kontrol.js
+++ assets/js/kontrol.js
@@ -65,8 +65,6 @@ firebase.auth().onAuthStateChanged(function (user) {
 
     });
 
-
-
     document.getElementById("pHForm").addEventListener("submit", function (event) {
       event.preventDefault(); // Prevent form from refreshing the page
       const pHValue = parseInt(document.getElementById("pHInput").value); // Convert to integer
@@ -134,7 +132,114 @@ firebase.auth().onAuthStateChanged(function (user) {
             }
         });
     });
-  
+
+
+    var RefStatus = firebase.database().ref("KONTROL/MODE");
+    var KontrolCheckbox = document.getElementById("Kontrol");
+    KontrolCheckbox.addEventListener("change", function () {
+      RefStatus.set(KontrolCheckbox.checked ? 1 : 2);
+    });
+
+    RefStatus.on("value", function (snapshot) {
+      var value = snapshot.val();
+      document.getElementById("Kontrol").checked = value === 1;
+      console.log(value);
+      if (value == 1) {
+        document.getElementById("WaterpumpCheckbox").disabled = true;
+        document.getElementById("start-time").disabled = false;
+        document.getElementById("end-time").disabled = false;
+        document.getElementById("save-btn").disabled = false;
+      }
+      else{
+        document.getElementById("save-btn").disabled = true;
+        document.getElementById("start-time").disabled = true;
+        document.getElementById("end-time").disabled = true;
+        document.getElementById("WaterpumpCheckbox").disabled = false;
+        document.getElementById("WaterpumpCheckbox").disabled = false;
+      }
+    });
+
+    var RefPWaterpump = firebase.database().ref("KONTROL/Waterpump");
+    var WaterpumpCheckbox = document.getElementById("WaterpumpCheckbox");
+    WaterpumpCheckbox.addEventListener("change", function () {
+      RefPWaterpump.set(WaterpumpCheckbox.checked ? 1 : 0);
+    });
+    RefPWaterpump.on("value", function (snapshot) {
+      var value = snapshot.val();
+      document.getElementById("WaterpumpCheckbox").checked = value === 1;
+      document.getElementById("StatusWaterpump").textContent =
+        value === 1 ? "Terbuka" : "Tertutup";
+    });
+
+
+
+    var TimeRef = firebase.database().ref("KONTROL");
+    TimeRef.on("value", function (snapshot) {
+      // Get the latest value from the database
+
+      var datajadwal = snapshot.val();
+      
+
+      document.getElementById("start-time").value = datajadwal.Start_time;
+      document.getElementById("end-time").value = datajadwal.End_time;
+    });
+
+    
+    
+
+    document.getElementById("save-btn").addEventListener("click", function() {
+      var Start_time = document.getElementById("start-time").value;
+      var End_time = document.getElementById("end-time").value;
+   
+      // Menampilkan konfirmasi SweetAlert
+      Swal.fire({
+         icon: 'question',
+         title: 'Apakah Anda yakin?',
+         text: 'Anda akan menyimpan data Start Time dan End Time ke Firebase.',
+         showCancelButton: true,
+         confirmButtonText: 'Ya, Simpan',
+         cancelButtonText: 'Batal',
+      }).then((result) => {
+         if (result.isConfirmed) {
+            // Jika pengguna memilih 'Ya, Simpan'
+            firebase.database().ref("KONTROL/Start_time").set(Start_time);
+            firebase.database().ref("KONTROL/End_time").set(End_time);
+   
+            var timeArray = Start_time.split(":");
+            var hour = parseInt(timeArray[0]);
+            var minute = parseInt(timeArray[1]);
+   
+            var timeArray2 = End_time.split(":");
+            var hour2 = parseInt(timeArray2[0]);
+            var minute2 = parseInt(timeArray2[1]);
+   
+            firebase.database().ref("KONTROL/Jam_Mulai").set(hour);
+            firebase.database().ref("KONTROL/Menit_Mulai").set(minute);
+   
+            firebase.database().ref("KONTROL/Jam_Selesai").set(hour2);
+            firebase.database().ref("KONTROL/Menit_Selesai").set(minute2);
+   
+            // Tampilkan konfirmasi bahwa data berhasil disimpan
+            Swal.fire({
+               icon: 'success',
+               title: 'Data berhasil disimpan!',
+               text: 'Start Time dan End Time telah berhasil disimpan ke Firebase.',
+               confirmButtonText: 'OK'
+            });
+         } else {
+            // Jika pengguna memilih 'Batal'
+            Swal.fire({
+               icon: 'info',
+               title: 'Proses dibatalkan',
+               text: 'Data tidak disimpan.',
+            });
+         }
+      });
+    });
+
+
+
+   
 
 
 
